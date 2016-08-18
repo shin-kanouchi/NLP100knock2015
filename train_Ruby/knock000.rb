77cded455ecccb01c54b29f208479e5aa631d30a
shin-kanouchi <tenzyonerai@gmail.com>
2016-08-18 16:27:05 +0900

    start Ruby training

---
+++ train_Ruby/knock000.rb
@@ -0,0 +1,12 @@
+#-*-coding:utf-8-*-
+#2016/08/17 02:58:05 Shin Kanouchi
+
+#00. 文字列の逆順
+#文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ
+
+str1 = "stressed"
+puts str1.reverse
+
+#print 'hello (print)' # 改行なしで出力
+#puts 'hello(put)' # 改行ありで出力
+#p 'hello(p)' # デバッグ用出力（データ形式がわかる）
